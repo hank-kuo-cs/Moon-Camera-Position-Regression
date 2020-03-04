984f43236ddddf42fc5ad6d08b99d7837d963920
Hank <52442723+hank-kuo-cs@users.noreply.github.com>
2020-03-04 16:16:51 +0800

    Test/regression old dataset (#5)
    * test old dataset
    
    * fix data loading
    
    * adjust show train loss
    
    * fix train

--- Regression/network/train.py
+++ Regression/network/train.py
@@ -11,8 +11,11 @@ class TrainNetwork(Network):
                          optimizer=optimizer,
                          tensorboard_writer=tensorboard_writer,
                          epoch=epoch)
+        self.show_loss = 0.0
 
     def run_one_epoch(self):
+        self.model.train()
+
         for idx, (inputs, labels) in enumerate(self.get_data()):
             self.optimizer.zero_grad()
 
@@ -32,5 +35,8 @@ class TrainNetwork(Network):
         self._epoch += 1
 
     def show_step_loss(self, loss, step):
+        self.show_loss += loss
         if step % config.tensorboard.loss_step == 0:
-            logging.info('epoch %d, %d step, loss = %.6f' % (self._epoch, step, loss))
+            avg_step_loss = self.show_loss / config.tensorboard.loss_step
+            logging.info('epoch %d, %d step, loss = %.6f' % (self._epoch, step, avg_step_loss))
+            self.show_loss = 0
