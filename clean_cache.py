12dceabec968c7055d1af3f93d498e36982213df
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-03-05 16:47:10 +0800

    add clean cache.py

---
+++ clean_cache.py
@@ -0,0 +1,18 @@
+import os
+from glob import glob
+
+
+def recursive_travel_all_directories(dir_path):
+	subdir_paths = glob(dir_path + '*/')
+	print(subdir_paths)
+	if subdir_paths:
+		for subdir_path in subdir_paths:
+			if subdir_path.find('__pycache__') > 0:
+				os.system('rm -rf %s' % subdir_path)
+			else:
+				recursive_travel_all_directories(subdir_path)
+	else:
+		return
+
+
+recursive_travel_all_directories('./')
