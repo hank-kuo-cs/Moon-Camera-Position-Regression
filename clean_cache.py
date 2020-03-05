b65e27d2792216632be271fa19745aa8b889cf60
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-03-05 16:49:52 +0800

    add clean cache.py

--- clean_cache.py
+++ clean_cache.py
@@ -4,11 +4,11 @@ from glob import glob
 
 def recursive_travel_all_directories(dir_path):
 	subdir_paths = glob(dir_path + '*/')
-	print(subdir_paths)
 	if subdir_paths:
 		for subdir_path in subdir_paths:
-			if subdir_path.find('__pycache__') > 0:
+			if subdir_path.find('__pycache__') > 0 or subdir_path.find('.idea') > 0:
 				os.system('rm -rf %s' % subdir_path)
+				print('delete %s' % subdir_path)
 			else:
 				recursive_travel_all_directories(subdir_path)
 	else:
