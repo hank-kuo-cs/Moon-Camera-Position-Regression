refactor code & fix avg loss calculation

Regression/network/test.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from .network import Network
+from ..loss.mse import adjust_azim_labels_to_use_scmse
 from ..config import config
 from tqdm import tqdm
 
@@ -21,13 +22,13 @@ class TestNetwork(Network):
             for idx, (inputs, labels) in tqdm(enumerate(self.get_data())):
                 predicts = self.model(inputs)
 
-                predicts, labels = self.transform_spherical_angle_label(predicts, labels)
-
                 self.add_predicts(predicts)
                 self.add_labels(labels)
 
-                loss = self.loss_func(predicts.clone(), labels.clone())
+                loss = self.loss_func(predicts, labels)
                 self.avg_loss += loss.item()
+
+                del predicts, labels
 
         self.avg_loss /= (config.dataset.test_dataset_num // config.network.batch_size)
 
@@ -55,87 +56,85 @@ class TestNetwork(Network):
 
     def show_distance_error(self):
         dist_predicts = self.predicts[:, 0]
-        dist_labels = self.labels[:, 0]
+        dist_gts = self.labels[:, 0]
 
-        self.small_than_5km_indices = dist_labels <= 5
+        self.small_than_5km_indices = dist_gts <= 5
 
-        dist_avg_km_error = np.average(np.abs((dist_predicts - dist_labels)))
+        dist_avg_km_error = self.get_average_error(dist_predicts, dist_gts)
         print('Distance average error: ±%.3f km' % dist_avg_km_error)
 
-        dist_small_than_5km_predicts = dist_predicts[self.small_than_5km_indices]
-        dist_small_than_5km_labels = dist_labels[self.small_than_5km_indices]
-
-        dist_avg_small_than_5km_error = np.average(np.abs(dist_small_than_5km_predicts - dist_small_than_5km_labels))
+        dist_avg_small_than_5km_error = self.get_average_error(dist_predicts[self.small_than_5km_indices],
+                                                               dist_gts[self.small_than_5km_indices])
         print('Distance average error (<= 5km): ±%.3f km' % dist_avg_small_than_5km_error)
 
     def show_angle_error(self):
         if len(self.label_types) < 3:
             return
-        elev_predicts, elev_labels = self.predicts[:, 1], self.labels[:, 1]
-        azim_predicts, azim_labels = self.predicts[:, 2], self.labels[:, 2]
+        elev_predicts, elev_gts = self.predicts[:, 1], self.labels[:, 1]
+        azim_predicts, azim_gts = self.predicts[:, 2], self.labels[:, 2]
 
-        elev_avg_degree_error = np.average(np.abs(elev_predicts - elev_labels))
+        elev_avg_degree_error = self.get_average_error(elev_predicts, elev_gts)
         print('elev average error: ±%.3f degree' % elev_avg_degree_error)
 
-        azim_avg_degree_error = np.average(np.abs(azim_predicts - azim_labels))
+        azim_avg_degree_error = self.get_average_error(azim_predicts, azim_gts, is_azim=True)
         print('azim average error: ±%.3f degree' % azim_avg_degree_error)
 
-        elev_predicts_small_5km = elev_predicts[self.small_than_5km_indices]
-        elev_labels_small_5km = elev_labels[self.small_than_5km_indices]
+        elev_degree_error_small_5km = self.get_average_error(elev_predicts[self.small_than_5km_indices],
+                                                             elev_gts[self.small_than_5km_indices])
+        print('elev average error (dist <= 5km): ±%.3f degree' % elev_degree_error_small_5km)
 
-        elev_degree_error_small_5km = np.average(np.abs(elev_predicts_small_5km - elev_labels_small_5km))
-
-        azim_predicts_small_5km = azim_predicts[self.small_than_5km_indices]
-        azim_labels_small_5km = azim_labels[self.small_than_5km_indices]
-
-        azim_degree_error_small_5km = np.average(np.abs(azim_predicts_small_5km - azim_labels_small_5km))
-
-        print('elev average error (dist <= 5km): ±%.3fkm' % elev_degree_error_small_5km)
-        print('azim average error (dist <= 5km): ±%.3fkm' % azim_degree_error_small_5km)
+        azim_degree_error_small_5km = self.get_average_error(azim_predicts[self.small_than_5km_indices],
+                                                             azim_gts[self.small_than_5km_indices],
+                                                             is_azim=True)
+        print('azim average error (dist <= 5km): ±%.3f degree' % azim_degree_error_small_5km)
 
     def show_point_error(self):
-        if len(self.label_types) > 3:
-            for i in range(3, len(self.label_types)):
-                point_predicts = self.predicts[:, i]
-                point_labels = self.labels[:, i]
+        if len(self.label_types) <= 3:
+            return
 
-                point_avg_km_error = np.average(np.abs(point_predicts - point_labels))
-                print('%s average error: ±%.3f' % (self.label_types[i], point_avg_km_error))
+        for i in range(3, len(self.label_types)):
+            point_predicts = self.predicts[:, i]
+            point_gts = self.labels[:, i]
+
+            point_avg_km_error = self.get_average_error(point_predicts, point_gts)
+            print('%s average error: ±%.3f' % (self.label_types[i], point_avg_km_error))
 
     def add_predicts(self, predicts):
         predicts = self.tensor_to_numpy(predicts)
-        self.predicts = predicts if self.predicts is None else np.concatenate((self.predicts, predicts))
+        self.predicts.append(predicts)
 
     def add_labels(self, labels):
         labels = self.tensor_to_numpy(labels)
-        self.labels = labels if self.labels is None else np.concatenate((self.labels, labels))
+        labels = labels[:, :config.dataset.labels_num]
+        self.labels.append(labels)
 
     def normalize_predicts_and_labels(self):
+        self.predicts = np.concatenate(self.predicts)
+        self.labels = np.concatenate(self.labels)
+
         self.predicts[:, 0] *= config.generate.dist_between_moon_high_bound_km
         self.labels[:, 0] *= config.generate.dist_between_moon_high_bound_km
 
         if len(self.label_types) >= 3:
-            self.predicts[:, 1: 3] *= 360
-            self.labels[:, 1: 3] *= 360
+            # elev
+            self.predicts[:, 1] *= 90
+            self.labels[:, 1] *= 90
+
+            # azim
+            self.predicts[:, 2] *= 360
+            self.labels[:, 2] *= 360
 
         if len(self.label_types) > 3:
             self.predicts[:, 3:] /= config.dataset.normalize_point_weight
             self.labels[:, 3:] /= config.dataset.normalize_point_weight
 
     @staticmethod
-    def transform_spherical_angle_label(predicts, labels):
-        if len(config.dataset.labels) < 2:
-            return predicts, labels
+    def get_average_error(predicts, ground_truths, is_azim=False):
+        assert isinstance(predicts, np.ndarray) and isinstance(ground_truths, np.ndarray)
+        assert predicts.shape == (config.network.batch_size, 1)
+        assert ground_truths.shape == (config.network.batch_size, 1)
 
-        tmp = torch.zeros((config.network.batch_size, 2), dtype=torch.float).to(config.cuda.device)
+        if is_azim:
+            ground_truths = adjust_azim_labels_to_use_scmse(predicts, ground_truths)
 
-        predicts[:, 1: 3] = torch.remainder(predicts[:, 1: 3], 1)
-
-        over_one_radius_indices = torch.abs(predicts[:, 1:3] - labels[:, 1:3]) > 0.5
-
-        tmp[over_one_radius_indices & (labels[:, 1:3] < predicts[:, 1:3])] = 1
-        tmp[over_one_radius_indices & (labels[:, 1:3] >= predicts[:, 1:3])] = -1
-
-        labels[:, 1:3] += tmp
-
-        return predicts, labels
+        return float(np.average(np.abs(predicts - ground_truths)))