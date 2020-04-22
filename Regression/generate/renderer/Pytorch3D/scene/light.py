703de5b948726d421e9ad4a06871f9f330162c8b
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-04-23 03:24:27 +0800

    increase light density

--- Regression/generate/renderer/Pytorch3D/scene/light.py
+++ Regression/generate/renderer/Pytorch3D/scene/light.py
@@ -5,6 +5,6 @@ from .....config import config
 def load_lights():
     return DirectionalLights(device=config.cuda.device,
                              direction = ((-40.0, 200.0, 100.0),),
-                             ambient_color=((0.5, 0.5, 0.5),),
-                             diffuse_color=((0.5, 0.5, 0.5),),
+                             ambient_color=((0.7, 0.7, 0.7),),
+                             diffuse_color=((0.8, 0.8, 0.8),),
                              specular_color=((0.0, 0.0, 0.0),))
